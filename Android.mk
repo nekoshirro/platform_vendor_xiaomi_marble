817a987ef373d985474a313e8dc93c110ff5c828
Fiqri Ardyansyah <fiqri191002@gmail.com>
2025-11-02 01:48:28 +0000

    marble: Drop stock firmware images
    Change-Id: I9502e0b4ef2503ecb99b83703c89fbf75f1a423e
    Signed-off-by: Fiqri Ardyansyah <fiqri191002@gmail.com>

--- Android.mk
+++ Android.mk
@@ -3,28 +3,3 @@
 #
 
 LOCAL_PATH := $(call my-dir)
-
-ifeq ($(TARGET_DEVICE),marble)
-
-$(call add-radio-file-sha1-checked,radio/abl.img,39c48c1fecf6293539ef3370038e694624fdf11c)
-$(call add-radio-file-sha1-checked,radio/aop.img,bc423ef94916a4e3801c7699172aa7ad4c52c825)
-$(call add-radio-file-sha1-checked,radio/aop_config.img,cab2729326f534bdf3f53cda7489372c74c46320)
-$(call add-radio-file-sha1-checked,radio/bluetooth.img,fa6445094bcb580e838365a35de06065d1a311ae)
-$(call add-radio-file-sha1-checked,radio/cpucp.img,d7248c09103b39c47f90466aef076401d07f5fec)
-$(call add-radio-file-sha1-checked,radio/devcfg.img,828e722aaf1b339305e848ba20e1669f36278698)
-$(call add-radio-file-sha1-checked,radio/dsp.img,abb0cf432db0d513601b5314b55b1765dc900317)
-$(call add-radio-file-sha1-checked,radio/featenabler.img,f2da269cb55c193355c32db7fb7276f53a93c4e5)
-$(call add-radio-file-sha1-checked,radio/hyp.img,db38eae3743f0237930d42b938a579d7128edfec)
-$(call add-radio-file-sha1-checked,radio/imagefv.img,b236bf6dda6d947dd73113ebafdb4149e7ed8d06)
-$(call add-radio-file-sha1-checked,radio/keymaster.img,6be1f97503d789b0154c85c45d847ed050dc7166)
-$(call add-radio-file-sha1-checked,radio/modem.img,731514a2f519c81d5dab6c0f77ee3f80e1f922d9)
-$(call add-radio-file-sha1-checked,radio/qupfw.img,76bcf25dc10196f9176ea513d5c4339999822c2a)
-$(call add-radio-file-sha1-checked,radio/shrm.img,69f446f915161c12bf619a168d9e1d7a436360c8)
-$(call add-radio-file-sha1-checked,radio/tz.img,50534127337b06659c5ac36ec5e761fbce4999a0)
-$(call add-radio-file-sha1-checked,radio/uefi.img,e4f6c2dfe03c553e8e4c83056ae42e70c6167de2)
-$(call add-radio-file-sha1-checked,radio/uefisecapp.img,5f8cd5d20d21cd8da88903dc08e7f3b14c14fa0b)
-$(call add-radio-file-sha1-checked,radio/xbl.img,9c14e53b56088352e4683133ed3afb953549d27e)
-$(call add-radio-file-sha1-checked,radio/xbl_config.img,ef26dffd3f3901f263b8870751bc290eeb91bd7e)
-$(call add-radio-file-sha1-checked,radio/xbl_ramdump.img,2662b22c80e392ac2bea8209d355699efee781f7)
-
-endif
